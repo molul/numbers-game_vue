15c844857fecab1e205b638ec4f1a1a7505a88bf
molul <luismorcillo@gmail.com>
2023-09-08 16:03:32 +0200

    Game ported from Vue to Vite

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+	<div v-if="status === 'beginning'" class="bg-red-400">
+		<Start @start-game="startGame"/>
+	</div>
+	<div v-if="status === 'gaming'" class="height-100 d-flex justify-content-center align-items-center">
+		<Game :size="size" @stop-game="stopGame"/>
+	</div>
+	<div v-if="status === 'stopping'" class="height-100 d-flex justify-content-center align-items-center">
+		<PlayAgain :score="score" :record="record" @play-again="playAgain"/>
+	</div>
+</template>
+
+<script>
+import Start from './components/Start.vue';
+import Game from './components/Game.vue';
+import PlayAgain from './components/PlayAgain.vue';
+
+export default {
+	name: "AppComponent",
+	components: { Start, Game, PlayAgain },
+	data (){
+		return {
+			size: 3,
+			status: 'beginning',
+			score: 0,
+			record: Infinity,
+		};
+	},
+	methods: {
+		startGame(size) {
+			this.size = size;
+			this.status = 'gaming';
+		},
+		stopGame(score) {
+			this.status = 'stopping';
+			this.score = score;
+			if (this.score < this.record) {
+				this.record = this.score
+			}
+		},
+		playAgain () {
+			this.status = 'gaming';
+		}
+	}
+}
+</script>
+
+<style>
+.height-100 {
+	height: 100vh;
+}
+
+</style>
---
+++ src/components/PlayAgain.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="text-center">
+		<div>
+			<h3>Time: <Time :time="score" /></h3>
+			<h3>Best time: <Time :time="record" /></h3>
+		</div>
+		<div>
+			<button class="btn btn-secondary mt-4" @click="$emit('play-again')">Play again</button>
+		</div>
+	</div>
+	
+</template>
+
+<script>
+import Time from './Time.vue';
+
+export default {
+	name: 'PlayAgainComponent',
+	props: [ 'score', 'record' ],
+	components: {Time},
+}
+</script>
+
+<style scoped>
+h3 {
+	margin: 0;
+}
+</style>
