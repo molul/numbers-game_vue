3ac8b1429e89c9876cc925d2320e4c8edc3397c6
molul <luismorcillo@gmail.com>
2023-09-08 16:36:49 +0200

    New main menu

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -1,20 +1,22 @@
 <template>
-	<div class="text-center py-4 rounded-lg bg-slate-300">
-		<div class="text-xl font-bold mb-2">Choose level</div>
-		<div>
-			<input type="radio" name="level" id="easy" :value="3" v-model="level" checked />
-			<label for="easy" class="me-4">Easy</label>
+	<div class="text-center py-6 rounded-lg bg-slate-300">
+		<div class="text-xl font-bold mb-4">Choose level</div>
+		
+		<div class="px-2 sm:px-4 space-y-6">
+			<div class="">
+				<button class="w-2/3 h-14 text-sm sm:text-base shadow-md transition-colors px-6 py-2 rounded-md bg-slate-500 hover:bg-slate-400 active:bg-slate-400 text-white " @click="$emit('start-game', 3)">Easy (3x3)</button>
+			</div>
 
-			<input type="radio" name="level" :value="4" id="medium" v-model="level"  />
-			<label for="medium" class="me-4">Medium</label>
+			<div class="">
+				<button class="w-2/3 h-14 text-sm sm:text-base shadow-md transition-colors px-6 py-2 rounded-md bg-slate-500 hover:bg-slate-400 active:bg-slate-400 text-white " @click="$emit('start-game', 4)">Medium (4x4)</button>
+			</div>
 
-			<input type="radio" name="level" :value="5" id="hard" v-model="level" />
-			<label for="hard">Hard</label>
+			<div class="">
+				<button class="w-2/3 h-14 text-sm sm:text-base shadow-md transition-colors px-6 py-2 rounded-md bg-slate-500 hover:bg-slate-400 active:bg-slate-400 text-white " @click="$emit('start-game', 5)">Hard (5x5)</button>
+			</div>
 		</div>
+		
 
-		<div class="mt-4">
-			<button class="text-sm sm:text-base shadow-md transition-colors px-6 py-2 rounded-md bg-slate-500 hover:bg-slate-400 active:bg-slate-400 text-white " @click="$emit('start-game', this.level)">START</button>
-		</div>
 	</div>
 </template>
 
